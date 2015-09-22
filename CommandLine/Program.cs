933d371e7ee08d99fcffbbcfc5b27f2a2ccf2cf7
goddd92 <goddd92@gmail.com>
2015-09-22 15:11:28 +0300

    Start

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -10,6 +10,44 @@ namespace CommandLine
     {
         static void Main(string[] args)
         {
+            string data = "";
+            CommandInterface command = new App();
+            data = Console.ReadLine();
+            string[] commands = data.Split(' ');
+            int result = data.CompareTo("app start");
+        }
+    }
+
+
+    interface CommandInterface
+    {
+        void execute();
+    }
+
+    class App: CommandInterface
+    {
+        public App ()
+        {
+            Start start = new Start();
+            Stop stop = new Stop();
+        }
+        void CommandInterface.execute()
+        {
+
+        }
+    }
+    class Start: CommandInterface
+    {
+        void CommandInterface.execute()
+        {
+           Console.WriteLine("Hello Kitty");
+        }
+    }
+    class Stop : CommandInterface
+    {
+        void CommandInterface.execute()
+        {
+            Environment.Exit(0);
         }
     }
 }
